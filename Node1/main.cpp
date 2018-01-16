a6d83a7328037cc25d005f6706dba5284c587c40
Martin Kristien <martin.kristien.com>
2018-01-16 15:23:27 +0000

    -Adding base solution with one project

---
+++ Node1/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Node1.cpp
+ *
+ * Created: 16/01/2018 15:13:47
+ * Author : martin
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
